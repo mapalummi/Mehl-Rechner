520eb23c01e98e960ba4ec29cdf8ab0b91053195
Marco <marco.palummieri@gmail.com>
2025-04-15 22:55:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const flour = 0.57;
+let newAmountValue;
+
+function calculateFlour() {
+  getInput();
+  clearInput();
+  calculateAmount();
+}
+
+function getInput() {
+  let amountValue = document.getElementById('input_field').value;
+  newAmountValue = parseInt(amountValue);
+}
+
+function clearInput() {
+  document.getElementById('input_field').value = '';
+}
+
+function calculateAmount() {
+  let result = newAmountValue / flour;
+  let newResult = Math.round(result);
+
+  showResult(newResult);
+}
+
+function showResult(newResult) {
+  document.getElementById('result_field').innerHTML = '';
+  document.getElementById('result_field').innerHTML += /*html*/ `
+        <p>${newResult} Milliliter</p>
+    `;
+}
